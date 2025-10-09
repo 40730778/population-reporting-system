3d0efb83e25dc54d5e5dd33652541ada0d3385fd
Israel Ayemo <40730778@live.napier.ac.uk>
2025-10-09 17:06:07 +0100

    Create CountryReportService.java
    "Added CityReportService class for city report feature"

---
+++ src/main/java/src/main/java/.com.napier.sem/CountryReportService.java
@@ -0,0 +1,7 @@
+package com.populationreporting;
+
+public class CountryReportService {
+    public void generateCountryReport() {
+        System.out.println("Generating country report...");
+    }
+}
