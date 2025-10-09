40d2e320059f12f40e4b28b8e27231a2218b8a86
Israel Ayemo <40730778@live.napier.ac.uk>
2025-10-09 13:43:13 +0100

    "Added LanguageStatsService class for language statistics feature"

---
+++ src/main/java/com/napier/sem/LanguageStatsService.java
@@ -0,0 +1,8 @@
+package com.napier.sem;
+
+public class LanguageStatsService {
+    public void generateLanguageStatistics() {
+        System.out.println("Generating language statistics...");
+    }
+}
+
